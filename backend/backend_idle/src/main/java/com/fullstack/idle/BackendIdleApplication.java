9c43fd018332b56f4675df885570e53b87ee96f6
mineruwo <99243341+mineruwo@users.noreply.github.com>
2025-07-24 15:05:52 +0900

    backend 소스 셋업 (IDRP-7)

---
+++ backend/backend_idle/src/main/java/com/fullstack/idle/BackendIdleApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.idle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendIdleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendIdleApplication.class, args);
+	}
+
+}
