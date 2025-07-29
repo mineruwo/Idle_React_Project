d767189e02d80d28162a8a67da65e870e4036914
dongshinlee <fullstack.leened@naver.com>
2025-07-29 11:41:57 +0900

    implement appjs root 연결 (idpr-41)
    appjs 수정

---
+++ frontend/frontend_idle/src/mainpage/router/root.js
@@ -0,0 +1,15 @@
+import { lazy, Suspense } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const Loading = <div>Loading 중...</div>
+
+const Main = lazy(()=> import("../pages/MainPage"));
+
+const root = createBrowserRouter([
+    {
+        path:"",
+        element:<Suspense fallback={Loading}><Main/></Suspense>
+    }
+]);
+
+export default root;
