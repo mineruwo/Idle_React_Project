ed84945742fb8c81654756e32cb6ab1a93549b17
mineruwo <99243341+mineruwo@users.noreply.github.com>
2025-07-30 14:43:43 +0900

    modify 메인 이미지 확장자 수정 (idrp-41)

--- frontend/frontend_idle/src/layouts/components/mainpage/IntroComponent.js
+++ frontend/frontend_idle/src/layouts/components/mainpage/IntroComponent.js
@@ -5,7 +5,7 @@ const IntroComponent = () => {
     return (
         <div className="position-relative text-white">
             <img
-                src={process.env.PUBLIC_URL + "/img/main/MainImage.png"} 
+                src={process.env.PUBLIC_URL + "/img/main/MainImage.PNG"} 
                 alt="소개 이미지"
                 className="img-fluid w-100"
             />
