422d253aa6d170d5a21af19e098228ed407b320d
mineruwo <99243341+mineruwo@users.noreply.github.com>
2025-07-30 14:31:40 +0900

    modify main이미지 경로 수정 (idrp-41)

--- frontend/frontend_idle/src/layouts/components/mainpage/IntroComponent.js
+++ frontend/frontend_idle/src/layouts/components/mainpage/IntroComponent.js
@@ -5,7 +5,7 @@ const IntroComponent = () => {
     return (
         <div className="position-relative text-white">
             <img
-                src="/img/main/MainImage.png"
+                src={process.env.PUBLIC_URL + "/img/main/MainImage.png"} 
                 alt="소개 이미지"
                 className="img-fluid w-100"
             />
