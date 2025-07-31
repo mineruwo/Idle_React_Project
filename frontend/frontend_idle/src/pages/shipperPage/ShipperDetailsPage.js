745d056c81e9235022100646477c0f30864f3d87
CJH93 <fullstack.ehdrmf98@naver.com>
2025-07-31 18:31:11 +0900

    modify shipper 디자인 추가 구현 (idrp-78)
    포인트 페이지 추가

--- frontend/frontend_idle/src/pages/shipperPage/ShipperDetailsPage.js
+++ frontend/frontend_idle/src/pages/shipperPage/ShipperDetailsPage.js
@@ -1,14 +1,13 @@
 import ShipperDetailsComponent from "../../layouts/components/shipperComponent/ShipperDetailsComponent";
+import "../../theme/ShipperCustomCss/ShipperDetails.css";
 
 const ShipperDetailsPage = () => {
     return (
-        <div>
-            <h2>배송 상세 정보</h2>
-            <div>
-                <ShipperDetailsComponent />
-                <ShipperDetailsComponent />
-                <ShipperDetailsComponent />
-            </div>
+        <div className="delivery-detail-container">
+            <h2 className="page-title">배송 상세 정보</h2>
+            <ShipperDetailsComponent />
+            <ShipperDetailsComponent />
+            <ShipperDetailsComponent />
         </div>
     );
 };
