10090ef2357ff9657fb219566936c474f253ff1b
CJH93 <fullstack.ehdrmf98@naver.com>
2025-09-01 16:52:06 +0900

    Update App.js

--- frontend/frontend_idle/src/App.js
+++ frontend/frontend_idle/src/App.js
@@ -3,23 +3,23 @@ import root from "./router/root";
 import { Provider } from "react-redux";
 import store from "./store";
 
-// bootstrap 
+// bootstrap
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./theme/bootStrap.css" // 덮어쓸 스타일 파일
+import "./theme/bootStrap.css"; // 덮어쓸 스타일 파일
 import AuthProvider from "./auth/AuthProvider";
 import BubbleAnimation from "./layouts/components/carownerComponent/common/BubbleAnimation";
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <Provider store={store}>
-        <div className="app-root">
-          <BubbleAnimation className="bubble-layer" />
-          <div className="app-content">
-            <RouterProvider router={root} />
-          </div>
-        </div>
-      </Provider>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Provider store={store}>
+                <div className="app-root bg-secondary">
+                    <BubbleAnimation className="bubble-layer" />
+                    <div className="app-content">
+                        <RouterProvider router={root} />
+                    </div>
+                </div>
+            </Provider>
+        </AuthProvider>
+    );
 }
