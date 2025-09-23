a7c291629259a2e146cc6c674d826a8c9f78130f
CJH93 <fullstack.ehdrmf98@naver.com>
2025-09-23 14:36:03 +0900

    flutter init commit

---
+++ application/lib/const/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const PRIMARY_COLOR = Color.fromRGBO(136, 180, 183, 1.0);
+const SECOND_COLOR = Color.fromRGBO(185, 213, 217, 1.0);
+const THIRD_COLOR = Color.fromRGBO(240, 245, 249, 1.0);
+const FOURTH_COLOR = Color.fromRGBO(215, 220, 217, 1.0);
+const FIFTH_COLOR = Color.fromRGBO(135, 151, 150, 1.0);
